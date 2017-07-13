8518f72e4fe604345be73e283fa7eb6781124f17
pencilCool <yhtangcoder@gmail.com>
2017-07-13 12:23:42 +0800

    fix bug
    修复传入参数的显示

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ class ChatScreen extends React.Component {
     const { params } = this.props.navigation.state;
     return (
       <View>
-        <Text>Chat with Lucy</Text>
+        <Text>Chat with {params.user}</Text>
       </View>
     );
   }
