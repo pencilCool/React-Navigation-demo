7514ce042e1e86262a82b969d8d7384408f8d444
pencilCool <yhtangcoder@gmail.com>
2017-07-15 16:57:48 +0800

    TextInput demo

--- App.js
+++ App.js
@@ -1,79 +1,70 @@
 
-
 import React, { Component } from 'react';
-import { ScrollView,Text, View,StyleSheet,Slider,AppRegistry,NavigatorIOS } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  AppRegistry,
+  View,
+  TextInput, } from 'react-native';
 
-var List = React.createClass({
 
+var Search = React.createClass({
   render() {
-    // const { navigate } = this.props.navigation;
-    return(
-      <ScrollView style = {styles.flex}>
-        <Text style = {styles.list_item} onPress = {this.goTo} > pencilCool1 </Text>
-        <Text style = {styles.list_item} onPress = {this.goTo} > pencilCool2 </Text>
-        <Text style = {styles.list_item} onPress = {this.goTo} > pencilCool3 </Text>
-      </ScrollView>
-    )
-  },
-
-  goTo() {
-    this.props.navigator.push({
-        component: Detail,
-        title: '游轮详情',
-        rightButtonTitle: '购物',
-        onRightButtonPress: function(){
-          alert('进入我的购物车');
-        }
-    })
-  }
-});
-
-
-var Detail = React.createClass({
-  render(){
     return (
-      <ScrollView>
-        <Text> detail page </Text>
-        <Text> few information </Text>
-      </ScrollView>
+      <View style = {[styles.flex, styles.flexDirection]}>
+        <View style = {styles.flex}>
+          <TextInput style = {styles.input} returnKeyType = "search"/>
+        </View>
+        <View style = {styles.btn}>
+          <Text style = {styles.search}>Search</Text>
+        </View>
+      </View>
     )
   }
 })
 
-
-export default class NavigatorIOSApp extends React.Component {
+var App = React.createClass({
   render() {
-    return (
-      <NavigatorIOS
-        initialRoute={{
-          component: List,
-          title: '游轮',
-          passProps:{},
-
-        }}
-        style={{flex: 1}}
-      />
-    )
-  }
-}
-
-
-var styles = StyleSheet.create({
-  flex: {
-    flex: 1
-  },
-  list_item: {
-    height:40,
-    marginLeft:50,
-    marginRight: 40,
-    borderBottomWidth:4,
-    borderBottomColor: '#dddd',
-    justifyContent:'center'
-  },
-  list_item_font: {
-    fontSize:18,
+      return (
+        <View style = {[styles.flex,styles.topStatus]}>
+          <Search> </Search>
+        </View>
+      )
   }
 });
 
+var styles = StyleSheet.create({
+  flex:{
+    flex:1
+  },
+  flexDirection: {
+    flexDirection: 'row'
+  },
+  topStatus:{
+    marginTop:25,
+  },
+  input: {
+    height:45,
+    borderWidth:1,
+    marginLeft:5,
+    paddingLeft:5,
+    borderColor:'#ccc',
+    borderRadius: 4
+  },
+  btn:{
+    width:55,
+    marginLeft: -5,
+    marginRight: 5,
+    backgroundColor: '#23BEFF',
+    height: 45,
+    justifyContent:'center',
+    alignItems: "center"
+  },
+  search: {
+    color: '#fff',
+    fontSize:15,
+    fontWeight:'bold'
+  }
+});
 
-AppRegistry.registerComponent('AwesomeProject', ()  => NavigatorIOSApp);
+AppRegistry.registerComponent('AwesomeProject',() => App)
