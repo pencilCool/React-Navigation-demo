b325fff441d334eaaabab4a97446053e74e3b25e
pencilCool <yhtangcoder@gmail.com>
2017-07-16 20:43:17 +0800

    geolocation

--- App.js
+++ App.js
@@ -1,72 +1,50 @@
-import React, { Component } from 'react';
-import { Alert, AppRegistry,NetInfo, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
+'use strict';
 
-export default class Touchables extends Component {
-  _onPressButton() {
-    NetInfo.isConnected.fetch().done((isConnected) => {
-      console.log('First, is ' + (isConnected ? 'online' : 'offline'));
-      alert('First, is ' + (isConnected ? 'online' : 'offline'))
-    });
-  }
+const React = require('react');
+const ReactNative = require('react-native');
+const {
+  AppRegistry,
+  CameraRoll,
+  Image,
+  Slider,
+  StyleSheet,
+  Switch,
+  Text,
+  View,
+  TouchableOpacity
+} = ReactNative;
 
-  _onLongPressButton() {
-    Alert.alert('You long-pressed the button!')
-  }
+var Geolocation = require('Geolocation')
 
-
+var App = React.createClass({
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableHighlight</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableOpacity onPress={this._onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableOpacity</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableNativeFeedback
-            onPress={this._onPressButton}
-            background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <TouchableWithoutFeedback
-            onPress={this._onPressButton}
-            >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-          </View>
-        </TouchableWithoutFeedback>
-        <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Touchable with Long Press</Text>
-          </View>
-        </TouchableHighlight>
+      <View>
+        <Text onPress = {this.vibration} style = {styles.btn} > </Text>
       </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 60,
-    alignItems: 'center'
+    )
   },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
-  },
-  buttonText: {
-    padding: 20,
-    color: 'white'
+  vibration() {
+    Geolocation.getCurrentPosition(function(data){
+      alert(JSON.stringify(data));
+    }, function() {
+      alert('get current postion');
+    })
   }
-})
+});
 
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => Touchables);
+var styles = StyleSheet.create({
+  btn: {
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
+    height: 35,
+    backgroundColor: '#3bc1ff',
+    color: '#fff',
+    lineHeight: 24,
+    fontWeight: 'bold',
+    textAlign:'center'
+  }
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => App);
