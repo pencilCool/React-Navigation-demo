36bf6420a4092dac0706fca9dfeb2d81178305e9
pencilCool <yhtangcoder@gmail.com>
2017-07-16 16:06:21 +0800

    PixelRatio

--- App.js
+++ App.js
@@ -14,65 +14,27 @@ AsyncStorage,
 TouchableOpacity,
 AlertIOS,
 ActionSheetIOS,
+PixelRatio,
 } from 'react-native';
 
 var App = React.createClass({
   render() {
     return (
       <View style = {styles.container}>
-        <Text style = {styles.item} onPress = {this.tip}>
-          showActionSheetWithOptions
-        </Text>
-        <Text style = {styles.item} onPress = {this.share}>
-          showShareActionSheetWithOptions
-        </Text>
+        <View style = {{borderWidth: 1,borderColor:'red',height:50,marginTop:100}}></View>
+        <View style = {{borderWidth: 1/PixelRatio.get(),borderColor: 'blue',height:100}}></View>
       </View>
+
     );
   },
-
-  tip() {
-    ActionSheetIOS.showActionSheetWithOptions({
-      options:[
-        'make a call ',
-        'send email',
-        'send message',
-        'cancel'
-      ],
-      cancelButtonIndex:3,
-      destructiveButtonIndex: 0,
-    },function(index){
-      alert(index);
-    });
-  },
-
-  share() {
-      ActionSheetIOS.showShareActionSheetWithOptions({
-        url:'https://code.facebook.com',
-      },function(err){
-        alert(err);
-      },function(e){
-        alert(e);
-      });
-
-  }
-
 });
 
 
 var styles = StyleSheet.create({
   container: {
     flex:1,
-    marginTop:25,
+    marginTop:100,
   },
-  item:{
-    marginTop:10,
-    marginLeft:5,
-    marginRight: 5,
-    height:30,
-    borderWidth:1,
-    padding:6,
-    borderColor: '#ddd'
-  }
 })
 
 
